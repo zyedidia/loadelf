c3b0f3ac255938e80cd71b332c52c9e1823a2f06
Zachary Yedidia <zyedidia@gmail.com>
2023-06-29 17:17:32 -0700

    Format

--- loader.c
+++ loader.c
@@ -1,95 +1,94 @@
-#include <fcntl.h>
-#include <stdlib.h>
 #include <elf.h>
+#include <fcntl.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/mman.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
-#define PAGE_SIZE	4096
-#define ALIGN		(PAGE_SIZE - 1)
-#define ROUND_PG(x)	(((x) + (ALIGN)) & ~(ALIGN))
-#define TRUNC_PG(x)	((x) & ~(ALIGN))
-#define PFLAGS(x)	((((x) & PF_R) ? PROT_READ : 0) | \
-			 (((x) & PF_W) ? PROT_WRITE : 0) | \
-			 (((x) & PF_X) ? PROT_EXEC : 0))
-#define LOAD_ERR	((unsigned long)-1)
+#define PAGE_SIZE 4096
+#define ALIGN (PAGE_SIZE - 1)
+#define ROUND_PG(x) (((x) + (ALIGN)) & ~(ALIGN))
+#define TRUNC_PG(x) ((x) & ~(ALIGN))
+#define PFLAGS(x)                                                     \
+    ((((x) &PF_R) ? PROT_READ : 0) | (((x) &PF_W) ? PROT_WRITE : 0) | \
+     (((x) &PF_X) ? PROT_EXEC : 0))
+#define LOAD_ERR ((unsigned long) -1)
 
-static int check_ehdr(Elf64_Ehdr *ehdr) {
-	unsigned char *e_ident = ehdr->e_ident;
-	return (e_ident[EI_MAG0] != ELFMAG0 || e_ident[EI_MAG1] != ELFMAG1 ||
-		e_ident[EI_MAG2] != ELFMAG2 || e_ident[EI_MAG3] != ELFMAG3 ||
-	    	e_ident[EI_CLASS] != ELFCLASS64 ||
-		e_ident[EI_VERSION] != EV_CURRENT ||
-		(ehdr->e_type != ET_EXEC && ehdr->e_type != ET_DYN)) ? 0 : 1;
+static int check_ehdr(Elf64_Ehdr* ehdr) {
+    unsigned char* e_ident = ehdr->e_ident;
+    return (e_ident[EI_MAG0] != ELFMAG0 || e_ident[EI_MAG1] != ELFMAG1 ||
+            e_ident[EI_MAG2] != ELFMAG2 || e_ident[EI_MAG3] != ELFMAG3 ||
+            e_ident[EI_CLASS] != ELFCLASS64 ||
+            e_ident[EI_VERSION] != EV_CURRENT ||
+            (ehdr->e_type != ET_EXEC && ehdr->e_type != ET_DYN))
+               ? 0
+               : 1;
 }
 
-static unsigned long loadelf_anon(int fd, Elf64_Ehdr *ehdr, Elf64_Phdr *phdr)
-{
-	unsigned long minva, maxva;
-	Elf64_Phdr *iter;
-	ssize_t sz;
-	int flags, dyn = ehdr->e_type == ET_DYN;
-	unsigned char *p, *base, *hint;
+static unsigned long loadelf_anon(int fd, Elf64_Ehdr* ehdr, Elf64_Phdr* phdr) {
+    unsigned long minva, maxva;
+    Elf64_Phdr* iter;
+    ssize_t sz;
+    int flags, dyn = ehdr->e_type == ET_DYN;
+    unsigned char *p, *base, *hint;
 
-	minva = (unsigned long)-1;
-	maxva = 0;
-	
-	for (iter = phdr; iter < &phdr[ehdr->e_phnum]; iter++) {
-		if (iter->p_type != PT_LOAD)
-			continue;
-		if (iter->p_vaddr < minva)
-			minva = iter->p_vaddr;
-		if (iter->p_vaddr + iter->p_memsz > maxva)
-			maxva = iter->p_vaddr + iter->p_memsz;
-	}
+    minva = (unsigned long) -1;
+    maxva = 0;
 
-	minva = TRUNC_PG(minva);
-	maxva = ROUND_PG(maxva);
+    for (iter = phdr; iter < &phdr[ehdr->e_phnum]; iter++) {
+        if (iter->p_type != PT_LOAD)
+            continue;
+        if (iter->p_vaddr < minva)
+            minva = iter->p_vaddr;
+        if (iter->p_vaddr + iter->p_memsz > maxva)
+            maxva = iter->p_vaddr + iter->p_memsz;
+    }
 
-	/* For dynamic ELF let the kernel chose the address. */	
-	hint = dyn ? NULL : (void *)minva;
-	flags = dyn ? 0 : MAP_FIXED;
-	flags |= (MAP_PRIVATE | MAP_ANONYMOUS);
+    minva = TRUNC_PG(minva);
+    maxva = ROUND_PG(maxva);
 
-	/* Check that we can hold the whole image. */
-	base = mmap(hint, maxva - minva, PROT_NONE, flags, -1, 0);
-	if (base == (void *)-1)
-		return -1;
-	munmap(base, maxva - minva);
+    // For dynamic ELF let the kernel chose the address.
+    hint = dyn ? NULL : (void*) minva;
+    flags = dyn ? 0 : MAP_FIXED;
+    flags |= (MAP_PRIVATE | MAP_ANONYMOUS);
 
-	flags = MAP_FIXED | MAP_ANONYMOUS | MAP_PRIVATE;
-	/* Now map each segment separately in precalculated address. */
-	for (iter = phdr; iter < &phdr[ehdr->e_phnum]; iter++) {
-		unsigned long off, start;
-		if (iter->p_type != PT_LOAD)
-			continue;
-		off = iter->p_vaddr & ALIGN;
-		start = dyn ? (unsigned long)base : 0;
-		start += TRUNC_PG(iter->p_vaddr);
-		sz = ROUND_PG(iter->p_memsz + off);
+    // Check that we can hold the whole image.
+    base = mmap(hint, maxva - minva, PROT_NONE, flags, -1, 0);
+    if (base == (void*) -1)
+        return -1;
+    munmap(base, maxva - minva);
 
-		p = mmap((void *)start, sz, PROT_WRITE, flags, -1, 0);
-		if (p == (void *)-1)
-			goto err;
-		if (lseek(fd, iter->p_offset, SEEK_SET) < 0)
-			goto err;
-		if (read(fd, p + off, iter->p_filesz) !=
-				(ssize_t)iter->p_filesz)
-			goto err;
-		mprotect(p, sz, PFLAGS(iter->p_flags));
-	}
+    flags = MAP_FIXED | MAP_ANONYMOUS | MAP_PRIVATE;
+    // Now map each segment separately in precalculated address.
+    for (iter = phdr; iter < &phdr[ehdr->e_phnum]; iter++) {
+        unsigned long off, start;
+        if (iter->p_type != PT_LOAD)
+            continue;
+        off = iter->p_vaddr & ALIGN;
+        start = dyn ? (unsigned long) base : 0;
+        start += TRUNC_PG(iter->p_vaddr);
+        sz = ROUND_PG(iter->p_memsz + off);
 
-	return (unsigned long)base;
+        p = mmap((void*) start, sz, PROT_WRITE, flags, -1, 0);
+        if (p == (void*) -1)
+            goto err;
+        if (lseek(fd, iter->p_offset, SEEK_SET) < 0)
+            goto err;
+        if (read(fd, p + off, iter->p_filesz) != (ssize_t) iter->p_filesz)
+            goto err;
+        mprotect(p, sz, PFLAGS(iter->p_flags));
+    }
+
+    return (unsigned long) base;
 err:
-	munmap(base, maxva - minva);
-	return LOAD_ERR;
+    munmap(base, maxva - minva);
+    return LOAD_ERR;
 }
 
 void trampoline(void*, void*, void*);
 
-static void fini(void) {
-}
+static void fini(void) {}
 
 int main(int host_argc, char* host_argv[], char* host_envp[]) {
     if (host_argc <= 1) {
@@ -134,42 +133,45 @@ int main(int host_argc, char* host_argv[], char* host_envp[]) {
 
     close(fd);
 
-    void *sp_base = calloc(1, 1 << 21);
-    unsigned long *sp = sp_base + (1 << 21) - 4096;
+    void* sp_base = calloc(1, 1 << 21);
+    unsigned long* sp = sp_base + (1 << 21) - 4096;
 
     (*sp) = host_argc - 1;
-    int argc = (int)*(sp);
-	char **argv = (char **)(sp + 1);
+    int argc = (int) *(sp);
+    char** argv = (char**) (sp + 1);
     for (int i = 0; i < host_argc - 1; i++) {
         argv[i] = host_argv[i + 1];
     }
     char **env, **p;
-	env = p = (char **)&argv[argc + 1];
-	while (*p++ != NULL)
+    env = p = (char**) &argv[argc + 1];
+    while (*p++ != NULL)
         ;
     while (*host_envp++ != NULL)
         ;
-    Elf64_auxv_t *av = (void *)p;
-    Elf64_auxv_t *host_av = (void *)host_envp;
+    Elf64_auxv_t* av = (void*) p;
+    Elf64_auxv_t* host_av = (void*) host_envp;
 
-    /* Reassign some vectors that are important for
-	 * the dynamic linker and for lib C. */
-#define AVSET(t, v, expr) case (t): (v)->a_un.a_val = (expr); break
-	while (host_av->a_type != AT_NULL) {
+    // Reassign some vectors that are important for the dynamic linker and for
+    // lib C.
+#define AVSET(t, v, expr)         \
+    case (t):                     \
+        (v)->a_un.a_val = (expr); \
+        break
+    while (host_av->a_type != AT_NULL) {
         *av = *host_av;
-		switch (av->a_type) {
-		AVSET(AT_PHDR, av, base + ehdrs.e_phoff);
-		AVSET(AT_PHNUM, av, ehdrs.e_phnum);
-		AVSET(AT_PHENT, av, ehdrs.e_phentsize);
-		AVSET(AT_ENTRY, av, entry);
-		AVSET(AT_EXECFN, av, (unsigned long)argv[0]);
-		AVSET(AT_BASE, av, av->a_un.a_val);
-		}
-		++av;
+        switch (av->a_type) {
+            AVSET(AT_PHDR, av, base + ehdrs.e_phoff);
+            AVSET(AT_PHNUM, av, ehdrs.e_phnum);
+            AVSET(AT_PHENT, av, ehdrs.e_phentsize);
+            AVSET(AT_ENTRY, av, entry);
+            AVSET(AT_EXECFN, av, (unsigned long) argv[0]);
+            AVSET(AT_BASE, av, av->a_un.a_val);
+        }
+        ++av;
         ++host_av;
-	}
+    }
 #undef AVSET
-	++av;
+    ++av;
 
     trampoline((void*) entry, sp, fini);
 
